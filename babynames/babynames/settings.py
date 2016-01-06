bf580639d375bce17763655d4f76b72d0081b115
Bartek Ciszkowski <bart.ciszk@gmail.com>
2016-01-05 21:13:18 -0500

    Use django-cors-headers for CORS control.

--- babynames/babynames/settings.py
+++ babynames/babynames/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
